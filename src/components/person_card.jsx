c328702c818e7998ea20bfd0f9bbecf7220c7c74
BrianaLentz <98199391+BrianaLentz@users.noreply.github.com>
2022-03-09 16:31:42 -0800

    putting it together

--- src/components/person_card.jsx
+++ src/components/person_card.jsx
@@ -1,13 +1,25 @@
 import React, {Component} from 'react'
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+            count: this.props.age
+        }
+    }
+    // ageCount = (e) => {
+    //     const count = this.state.count
+    //     this.setState(prev => ({age: prev.count + 1 }));
+    // }
     render(){
         const {lastName, firstName, age, hairColor} = this.props
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.count}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={()=> this.setState({count: this.state.count+1})}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
